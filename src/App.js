f296b8d6301bf477e5b6e5fedc2674f2446dd397
Ervin Kacar <kacarervin@gmail.com>
2022-09-12 10:11:44 +0200

    set redux store, and counter state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { useSelector } from "react-redux";
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   return (
     <div>
       <h1>Counter App</h1>
+      <h2>{counter}</h2>
     </div>
   );
 }
