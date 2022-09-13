391a7ba4ff0617090a4cc40a287ddd730f613925
Ervin Kacar <kacarervin@gmail.com>
2022-09-13 09:42:58 +0200

    Add redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,31 @@
 import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
 import { useSelector, useDispatch } from "react-redux";
+import { createAction } from "@reduxjs/toolkit";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment</button>
